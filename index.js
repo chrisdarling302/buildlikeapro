b1b302dde0253596beceb65b7316a04d7122df45
Chris <cldarling@gmail.com>
2025-03-16 16:43:53 -0400

    emailjs not loading fully

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
 </script>
 */
 
-function contact() {
-    event.preventDefault(event)
+function contact(event) {
+    event.preventDefault();
     /**emailjs
     .sendForm(
         "service_j0vole5",
         "template_m3odffw",
         event.target,
         "TfEdOzUw_Th6TcFQJ"
-    ).then(() =>{
+    ).then(() => {
         console.log("this worked")
     })*/
-const loading = document.querySelector(".modal__overlay--loading")
-const success = document.querySelector(".modal__overlay--success")
+const loading = document.querySelector(".modal__overlay--loading");
+const success = document.querySelector(".modal__overlay--success");
 loading.classList += " modal__overlay--visible"
 setTimeout(() =>{
     console.log("it worked 1")
