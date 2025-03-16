6d02c8299e0489a2bc4ce9b2a3b2812873949980
Chris <cldarling@gmail.com>
2025-03-16 16:30:18 -0400

    emailJS problem

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 //public key TfEdOzUw_Th6TcFQJ
 
 //Browser Script:
-//<script type="text/javascript"
-/**
+/**<script type="text/javascript"
 src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
 </script>
 <script type="text/javascript">
