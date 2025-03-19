835345a15e19e84ff5466dbc74eb25d248096966
Chris <cldarling@gmail.com>
2025-03-19 15:20:59 -0400

    background shapes complete

--- index.js
+++ index.js
@@ -16,7 +16,22 @@
    })();
 </script>
 */
+
+let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -28,8 +43,6 @@ function toggleContrast() {
     }
 }
 
-
-let isModalOpen = false;
  function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
